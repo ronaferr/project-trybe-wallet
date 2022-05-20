56c8d7a2fe0e471cc8baf223a0498355eb24c8a3
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-05-20 12:11:29 -0300

    implemetação de um "header" no componente "wallet" e resgate de email na store do redux

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { userState } = this.props;
+    return (
+      <section>
+        <header>
+          <p data-testid="email-field">{userState.email}</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
+      </section>);
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  userState: state.user });
+
+Wallet.propTypes = {
+  userState: propTypes.shape({
+    email: propTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default connect(mapStateToProps)(Wallet);
