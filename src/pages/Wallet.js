d259e73f341e763912aaf21f81ad5640091b0f5f
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-05-20 17:23:43 -0300

    implemetação de formulario de despesa e resgate de currencies da store e implematção no formulário

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -10,7 +10,7 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { userState } = this.props;
+    const { userState, currencies } = this.props;
     return (
       <section>
         <header>
@@ -18,18 +18,53 @@ class Wallet extends React.Component {
           <p data-testid="total-field">0</p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
+        <form>
+          <fieldset>
+            <input
+              type="number"
+              data-testid="value-input"
+            />
+            <input
+              type="text"
+              data-testid="description-input"
+            />
+            <label htmlFor="Moeda">
+              Moeda
+              <select id="Moeda">
+                {currencies.map((currency) => (
+                  (<option key={ currency }>{currency}</option>)
+                ))}
+              </select>
+            </label>
+            <select data-testid="method-input">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+            <select data-testid="tag-input">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </fieldset>
+        </form>
       </section>);
   }
 }
 
 const mapStateToProps = (state) => ({
-  userState: state.user });
+  userState: state.user,
+  currencies: state.wallet.currencies,
+});
 
 Wallet.propTypes = {
   userState: propTypes.shape({
     email: propTypes.string.isRequired,
   }).isRequired,
   dispatch: propTypes.func.isRequired,
+  currencies: propTypes.arrayOf(propTypes.string.isRequired).isRequired,
 };
 
 export default connect(mapStateToProps)(Wallet);
